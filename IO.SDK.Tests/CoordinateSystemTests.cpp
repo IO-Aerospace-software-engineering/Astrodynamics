f86161bfc027481c1019da25d815948dc3a068c2
sylvain-guillet <sylvain.guillet@live.com>
2021-11-23 15:41:18 +0000

    Add coverag report (#60)

--- IO.SDK.Tests/CoordinateSystemTests.cpp
+++ IO.SDK.Tests/CoordinateSystemTests.cpp
@@ -4,4 +4,11 @@
 TEST(CoordinateSystem, ToCharArray)
 {
     ASSERT_STREQ("PLANETOGRAPHIC", IO::SDK::CoordinateSystem::Planetographic().ToCharArray());
+    ASSERT_STREQ("CYLINDRICAL", IO::SDK::CoordinateSystem::Cylindrical().ToCharArray());
+    ASSERT_STREQ("GEODETIC", IO::SDK::CoordinateSystem::Geodetic().ToCharArray());
+    ASSERT_STREQ("LATITUDINAL", IO::SDK::CoordinateSystem::Latitudinal().ToCharArray());
+    ASSERT_STREQ("PLANETOGRAPHIC", IO::SDK::CoordinateSystem::Planetographic().ToCharArray());
+    ASSERT_STREQ("RA/DEC", IO::SDK::CoordinateSystem::RA_DEC().ToCharArray());
+    ASSERT_STREQ("RECTANGULAR", IO::SDK::CoordinateSystem::Rectangular().ToCharArray());
+    ASSERT_STREQ("SPHERICAL", IO::SDK::CoordinateSystem::Spherical().ToCharArray());
 }
