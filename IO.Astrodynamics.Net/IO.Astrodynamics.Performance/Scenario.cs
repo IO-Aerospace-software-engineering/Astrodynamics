8fe484fbf1cc98d1abff7e5c13a571087d0e2da7
Sylvain Guillet <sylvain.guillet@live.com>
2024-08-23 18:33:34 +0200

    Adapt scenario

--- IO.Astrodynamics.Net/IO.Astrodynamics.Performance/Scenario.cs
+++ IO.Astrodynamics.Net/IO.Astrodynamics.Performance/Scenario.cs
@@ -1,5 +1,8 @@
 // Copyright 2023. Sylvain Guillet (sylvain.guillet@tutamail.com)
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Numerics;
 using BenchmarkDotNet.Attributes;
 using IO.Astrodynamics.Body;
@@ -42,17 +45,17 @@ public class Scenario
         Clock clk = new Clock("My clock", 256);
         Spacecraft spc = new Spacecraft(-1001, "MySpacecraft", 100.0, 10000.0, clk,
             new StateVector(new Vector3(6800000.0, 0.0, 0.0), new Vector3(0.0, 7656.2204182967143, 0.0), _earth, TimeSystem.Time.J2000TDB, Frames.Frame.ICRF));
-        _srp = new SolarRadiationPressure(spc, [_earth]);
-        _atm = new AtmosphericDrag(spc, _earth);
-        List<ForceBase> forces = new List<ForceBase>();
-        forces.Add(new GravitationalAcceleration(_sun));
-        forces.Add(new GravitationalAcceleration(_moon));
-        forces.Add(new GravitationalAcceleration(_earth));
-        forces.Add(new AtmosphericDrag(spc, _earth));
-        forces.Add(new SolarRadiationPressure(spc, [_earth]));
-        _integrator = new VVIntegrator(forces, TimeSpan.FromSeconds(1.0), new StateVector(new Vector3(6800000.0 - Random.Shared.NextDouble(), 0.0, 0.0),
-            new Vector3(0.0, 8000.0 - Random.Shared.NextDouble(), 0.0), _earth,
-            TimeSystem.Time.J2000TDB, Frame.ICRF));
+        // _srp = new SolarRadiationPressure(spc, [_earth]);
+        // _atm = new AtmosphericDrag(spc, _earth);
+        // List<ForceBase> forces = new List<ForceBase>();
+        // forces.Add(new GravitationalAcceleration(_sun));
+        // forces.Add(new GravitationalAcceleration(_moon));
+        // forces.Add(new GravitationalAcceleration(_earth));
+        // forces.Add(_atm);
+        // forces.Add(_srp);
+        // _integrator = new VVIntegrator(forces, TimeSpan.FromSeconds(1.0), new StateVector(new Vector3(6800000.0 - Random.Shared.NextDouble(), 0.0, 0.0),
+        //     new Vector3(0.0, 8000.0 - Random.Shared.NextDouble(), 0.0), _earth,
+        //     TimeSystem.Time.J2000TDB, Frame.ICRF));
         _spacecraftPropagator = new Propagator.SpacecraftPropagator(new Window(TimeSystem.Time.J2000TDB, TimeSystem.Time.J2000TDB + spc.InitialOrbitalParameters.Period()), spc,
             new[] { _moon, _earth, _sun }, true, true, TimeSpan.FromSeconds(1.0));
     }
