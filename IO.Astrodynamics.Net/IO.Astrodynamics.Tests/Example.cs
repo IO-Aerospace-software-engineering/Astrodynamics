f3edd750c9a1409b5c9c73277393d257f5ef0457
Sylvain Guillet <sylvain.guillet@live.com>
2024-08-29 18:11:07 +0200

    Add example

---
+++ IO.Astrodynamics.Net/IO.Astrodynamics.Tests/Example.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using IO.Astrodynamics.Body;
+using IO.Astrodynamics.SolarSystemObjects;
+using IO.Astrodynamics.TimeSystem;
+using Xunit;
+
+namespace IO.Astrodynamics.Tests;
+
+public class Example
+{
+    [Fact]
+    public void ReadEphemeris()
+    {
+        //In this example we want the ephemeris of the moon at epoch (2000-01-01T12:00Z) in ICRF frame with earth at center
+        API.Instance.LoadKernels(new DirectoryInfo("Data/SolarSystem"));//replace Data/SolarSytem by your kernel path
+        var earth = new CelestialBody(PlanetsAndMoons.EARTH);
+        var moon = new CelestialBody(PlanetsAndMoons.MOON);
+        var ephemerid = moon.GetEphemeris(new TimeSystem.Time(new DateTime(2000, 1, 1, 12, 0, 0), TimeFrame.UTCFrame), earth, Frames.Frame.ICRF, Aberration.None);
+    }
+}
