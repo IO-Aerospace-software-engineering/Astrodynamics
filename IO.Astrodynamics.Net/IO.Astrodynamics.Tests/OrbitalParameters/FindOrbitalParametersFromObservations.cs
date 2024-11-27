2e0e08818fcf7ecbdb72a9350707cab371b6e7a2
Sylvain Guillet <sylvain.guillet@live.com>
2024-11-27 18:01:22 +0100

    Add tests

--- IO.Astrodynamics.Net/IO.Astrodynamics.Tests/OrbitalParameters/FindOrbitalParametersFromObservations.cs
+++ IO.Astrodynamics.Net/IO.Astrodynamics.Tests/OrbitalParameters/FindOrbitalParametersFromObservations.cs
@@ -1,3 +1,6 @@
+using IO.Astrodynamics.Body;
+using IO.Astrodynamics.SolarSystemObjects;
+using IO.Astrodynamics.Surface;
 using Xunit;
 
 namespace IO.Astrodynamics.Tests.OrbitalParameters;
@@ -12,5 +15,16 @@ public class FindOrbitalParametersFromObservations
     [Fact]
     public void Create()
     {
+        Site site = new Site(13, "DSS-13", TestHelpers.EarthAtJ2000);
+        var e1 = new TimeSystem.Time(2024, 1, 1);
+        var e2 = new TimeSystem.Time(2024, 1, 2);
+        var e3 = new TimeSystem.Time(2024, 1, 3);
+        var mars = new CelestialBody(499);
+        var obs1 = mars.GetEphemeris(e1, site, Frames.Frame.ICRF, Aberration.LT);
+        var obs2 = mars.GetEphemeris(e2, site, Frames.Frame.ICRF, Aberration.LT);
+        var obs3 = mars.GetEphemeris(e3, site, Frames.Frame.ICRF, Aberration.LT);
+        var orbitalParams =
+            Astrodynamics.OrbitalParameters.OrbitalParameters.CreateFromObservation_Gauss(obs1.ToEquatorial(), obs2.ToEquatorial(), obs3.ToEquatorial(), site, Stars.SUN_BODY);
+        Assert.Equal(obs2.ToKeplerianElements(), orbitalParams.ToKeplerianElements());
     }
 }
