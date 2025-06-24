30d8ab61d556040235128d2514320f0a2c99ad20
Sylvain Guillet <sylvain.guillet@weda.fr>
2025-06-24 14:02:39 +0200

    Add time of flight methods to LambertSolver
    Updated `LambertSolver.cs` with two new methods:
    `XToTimeOfFlight` for computing time of flight based on
    the x parameter and number of revolutions, and
    `XToTimeOfFlightGeneral` for selecting the appropriate
    calculation method (Battin, Lagrange, or Lancaster).
    Code cleanup includes removal of unnecessary using
    directives and enhanced documentation with XML comments.

--- IO.Astrodynamics.Net/IO.Astrodynamics/Maneuver/LambertSolver.cs
+++ IO.Astrodynamics.Net/IO.Astrodynamics/Maneuver/LambertSolver.cs
@@ -1,5 +1,5 @@
-using System.Collections.Generic;
 using IO.Astrodynamics.Math;
+using System.Collections.Generic;
 
 namespace IO.Astrodynamics.Maneuver;
 
@@ -39,5 +39,87 @@ public class LambertSolver
         // Conversion de la logique du constructeur C++ ici
         // Utiliser Vector3.Normalize, Vector3.Cross, Vector3.Magnitude, etc.
         // Remplir V1, V2, X, Iters, Nmax
+    }
+
+    /// <summary>
+    /// Computes the time of flight for a given x parameter and number of revolutions in the Lambert problem.
+    /// </summary>
+    /// <param name="x">The x parameter (related to the geometry of the transfer).</param>
+    /// <param name="N">The number of full revolutions.</param>
+    /// <returns>The computed time of flight.</returns>
+    public double XToTimeOfFlight(double x, int N)
+    {
+        double a = 1.0 / (1.0 - x * x);
+        double tof;
+
+        if (a > 0) // Elliptic case
+        {
+            double alpha = 2.0 * System.Math.Acos(x);
+            double beta = 2.0 * System.Math.Asin(System.Math.Sqrt(m_lambda * m_lambda / a));
+            if (m_lambda < 0.0) beta = -beta;
+            tof = (a * System.Math.Sqrt(a) * ((alpha - System.Math.Sin(alpha)) - (beta - System.Math.Sin(beta)) + 2.0 * System.Math.PI * N)) / 2.0;
+        }
+        else // Hyperbolic case
+        {
+            double alpha = 2.0 * System.Math.Acosh(x);
+            double beta = 2.0 * System.Math.Asinh(System.Math.Sqrt(-m_lambda * m_lambda / a));
+            if (m_lambda < 0.0) beta = -beta;
+            tof = (-a * System.Math.Sqrt(-a) * ((beta - System.Math.Sinh(beta)) - (alpha - System.Math.Sinh(alpha)))) / 2.0;
+        }
+        return tof;
+    }
+
+    /// <summary>
+    /// Computes the time of flight for a given x parameter and number of revolutions using the appropriate method (Battin, Lagrange, or Lancaster).
+    /// </summary>
+    /// <param name="x">The x parameter (related to the geometry of the transfer).</param>
+    /// <param name="N">The number of full revolutions.</param>
+    /// <returns>The computed time of flight.</returns>
+    public double XToTimeOfFlightGeneral(double x, int N)
+    {
+        const double battinThreshold = 0.01;
+        const double lagrangeThreshold = 0.2;
+        double dist = System.Math.Abs(x - 1);
+
+        if (dist < lagrangeThreshold && dist > battinThreshold)
+        {
+            // Use Lagrange time of flight expression
+            return XToTimeOfFlight(x, N);
+        }
+
+        double K = m_lambda * m_lambda;
+        double E = x * x - 1.0;
+        double rho = System.Math.Abs(E);
+        double z = System.Math.Sqrt(1 + K * E);
+
+        if (dist < battinThreshold)
+        {
+            // Use Battin series time of flight expression
+            double eta = z - m_lambda * x;
+            double S1 = 0.5 * (1.0 - m_lambda - x * eta);
+            double Q = SpecialFunctions.Hypergeometric(S1, 1e-11);
+            Q = 4.0 / 3.0 * Q;
+            double tof = (eta * eta * eta * Q + 4.0 * m_lambda * eta) / 2.0 + N * System.Math.PI / System.Math.Pow(rho, 1.5);
+            return tof;
+        }
+        else
+        {
+            // Use Lancaster time of flight expression
+            double y = System.Math.Sqrt(rho);
+            double g = x * z - m_lambda * E;
+            double d;
+            if (E < 0)
+            {
+                double l = System.Math.Acos(g);
+                d = N * System.Math.PI + l;
+            }
+            else
+            {
+                double f = y * (z - m_lambda * x);
+                d = System.Math.Log(f + g);
+            }
+            double tof = (x - m_lambda * z - d / y) / E;
+            return tof;
+        }
     }
 }
